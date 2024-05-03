e287d6d16cfcd28b083b421e0ff7bb2221d559b5
ilham barahamou <ilham22barahamou@gmail.com>
2024-05-03 17:27:20 +0100

    full table

--- frontendBC/src/app/Fournisseur/Fournisseur.model.ts
+++ frontendBC/src/app/Fournisseur/Fournisseur.model.ts
@@ -1,5 +1,9 @@
 export class Fournisseur {
     fournisseurId!: number; // This can be undefined if not set
     nom!: string; // This can be undefined if not set
+    email!: string;
+    telephone!: string;
+    adresse!: string;
+    designation!: string;
   }
   
